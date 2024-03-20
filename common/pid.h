a01fc785a017b6dfe635d112fee932150dbd639b
Danila Vershinin <ciapnz@gmail.com>
2024-03-21 01:05:49 +0800

    Update pid.h

--- common/pid.h
+++ common/pid.h
@@ -29,14 +29,14 @@
 struct process_id {
   unsigned ip;
   short port;
-  unsigned int pid;
+  unsigned short pid;
   int utime;
 };
 
 struct process_id_ext {
   unsigned ip;
   short port;
-  unsigned int pid;
+  unsigned short pid;
   int utime;
   int actor_id;
 };
